56408de5588494743de468f85babdd1be1466792
Mission521 <2496562826@qq.com>
2026-02-04 11:46:30 +0800

    chore: ignore supabase migrations

--- supabase/migrations/20240523000003_add_category.sql
+++
@@ -1,3 +0,0 @@
--- Add category column to clockins table
-ALTER TABLE clockins
-ADD COLUMN category VARCHAR(50) DEFAULT '日常';
--- supabase/migrations/20240523000005_remove_unique_constraint.sql
+++
@@ -1,2 +0,0 @@
--- Drop the unique constraint that limits users to one clock-in per day
-ALTER TABLE clockins DROP CONSTRAINT IF EXISTS unique_user_date;
